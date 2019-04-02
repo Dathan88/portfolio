f54c32ed06f60d13fff6cd6c58b3ba32ff843ee9
Dathan Spencer <dpspencer88@gmail.com>
2019-04-02 11:10:23 -0600

    Change file or export names

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -5,12 +5,11 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const HomePage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Dathan Spencer</h1>
+    <p>Welcome to my Gatsby site.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
@@ -18,4 +17,4 @@ const IndexPage = () => (
   </Layout>
 )
 
-export default IndexPage
+export default HomePage

3bdb7a771f2e25010dd876f8d654cb290874f824
Dathan Spencer <dpspencer88@gmail.com>
2019-04-03 14:41:39 -0600

    Edit or delete defaults

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Personal Portfolio Webpage`,
+    title: `Dathan Spencer`,
     description: `Webpage to any personal Javascript related projects I have completed.`,
     author: `Dathan Spencer`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
     `gatsby-plugin-catch-links`,
     `gatsby-transformer-remark`,
       {
@@ -18,8 +19,8 @@ module.exports = {
       {
         resolve: `gatsby-source-filesystem`,
         options: {
-          name: `posts`,
-          path: `${__dirname}/src/posts`,
+          name: `pages`,
+          path: `${__dirname}/src/pages`,
         },
       },
     `gatsby-transformer-sharp`,
@@ -33,7 +34,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality

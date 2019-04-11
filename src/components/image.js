47952b8f90951dacabe69a419f96fa571bd08081
Dathan Spencer <dpspencer88@gmail.com>
2019-04-11 10:51:11 -0600

    Minor changes for custom img

--- src/components/image.js
+++ src/components/image.js
@@ -6,16 +6,17 @@ const Image = () => (
   <StaticQuery
     query={graphql`
       query {
-        placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+        fileName: file(relativePath: { eq: "Dathan2.2.min.png" }) {
           childImageSharp {
-            fluid(maxWidth: 300) {
+            fluid(maxWidth: 600) {
               ...GatsbyImageSharpFluid
             }
           }
         }
       }
     `}
-    render={data => <Img fluid={data.placeholderImage.childImageSharp.fluid} />}
+    render={data => <Img fluid={data.fileName.childImageSharp.fluid} />}
+    
   />
 )
 export default Image

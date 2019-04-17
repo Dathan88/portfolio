15e10dfeb04037fac3507ee22a7494a431aa9b4e
Dathan Spencer <dpspencer88@gmail.com>
2019-04-17 16:15:23 -0600

    About page and responsiveness

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,32 +1,59 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const AboutPage = () => (
   <Layout>
     <SEO title="About Me" />
-    <h1>About Me</h1>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi fugit
-      corporis, ipsam magni error laboriosam. Odit praesentium numquam quas
-      ipsam nostrum quaerat quae itaque, aliquid labore voluptatibus! Atque,
-      tenetur quod!
-    </p>
-    <p>
-      Occaecat laboris occaecat eu ullamco officia consectetur aliqua Lorem
-      ipsum ut anim. Laborum qui eiusmod elit quis aute. Ex eu adipisicing ipsum
-      mollit voluptate minim ut officia voluptate aute consectetur.
-    </p>
-    <p>
-      Quis nisi officia deserunt magna eu occaecat dolor proident esse. Eiusmod
-      amet nostrud tempor eiusmod ullamco excepteur culpa. Voluptate non esse
-      magna adipisicing. Dolor qui ea cillum ullamco qui cillum Lorem incididunt
-      laboris. Adipisicing elit irure veniam amet ea in occaecat qui mollit
-      pariatur do.
-    </p>
-    <Link to="/">Go back to the homepage</Link>
+    <main id="about">
+      <h1 className="lg-heading">
+        About <span className="text-secondary">Me</span>
+      </h1>
+      <h2 className="sm-heading">Let me tell you a few things...</h2>
+      <div className="about-info">
+        <div alt="personal photo" className="bio-image portriat" />
+        <div className="bio">
+          <h3 className="text-secondary">BIO</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Perferendis, error! Atque fugit non beatae vero, repudiandae hic
+            sunt odit dolore sed eos similique nobis maxime tenetur! Esse itaque
+            unde animi.
+          </p>
+        </div>
+        <div className="job job-1">
+          <h3>Jakubowski and Sons</h3>
+          <h6>Front-End Developer</h6>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi fugit
+            corporis, ipsam magni error laboriosam. Odit praesentium numquam
+            quas ipsam nostrum quaerat quae itaque, aliquid labore voluptatibus!
+            Atque, tenetur quod!
+          </p>
+        </div>
+        <div className="job job-2">
+          <h3>Blick Group</h3>
+          <h6>Graphic Designer</h6>
+          <p>
+            Occaecat laboris occaecat eu ullamco officia consectetur aliqua
+            Lorem ipsum ut anim. Laborum qui eiusmod elit quis aute. Ex eu
+            adipisicing ipsum mollit voluptate minim ut officia voluptate aute
+            consectetur.
+          </p>
+        </div>
+        <div className="job job-3">
+          <h3>Zemlak - Stanton</h3>
+          <h6>Front-End Developer</h6>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque
+            quisquam enim labore quam unde tenetur eum, ratione magni, nobis qui
+            iste?
+          </p>
+        </div>
+      </div>
+      {/* <Link to="/">Go back to the homepage</Link> */}
+    </main>
   </Layout>
 )
 

753fe570584710547dd1ed81a70b38168adbd755
Dathan Spencer <dpspencer88@gmail.com>
2019-04-17 16:15:03 -0600

    Change footer and div inner css

--- src/components/layout.js
+++ src/components/layout.js
@@ -20,15 +20,13 @@ const Layout = ({ children }) => (
         <Header siteTitle={data.site.siteMetadata.title} />
         <div
           style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
+            height: `calc(100vh - 60px)`,
           }}
         >
-          <main>{children}</main>
-          <br />
-          <footer />
+          <main>
+            {children}
+            <footer>Copyright &copy; 2019</footer>
+          </main>
         </div>
       </>
     )}

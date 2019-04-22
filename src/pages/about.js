e2f21fef6dde1fd4c7fd48f49f74a8009be6ef44
Dathan Spencer <dpspencer88@gmail.com>
2019-04-22 15:09:45 -0600

    Input custom comps for bio/about/contact

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,9 +3,44 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import PageTitle from "../components/PageTitle"
 import SubTitle from "../components/SubTitle"
+import Jobs from "../components/Jobs"
+import Bio from "../components/Bio"
 
-const titleProps = ['About', 'Me'];
-const subProps = 'Let me tell you a few things...';
+const titleProps = ["About", "Me"]
+const subProps = "Let me tell you a few things..."
+const jobProps = [
+  {
+    class: "job job-1",
+    company: "Jakubowski Inc.",
+    job: "Front-End Developer",
+    summary: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi fugit corporis, ipsam magni error laboriosam. Odit praesentium numquam quas ipsam nostrum quaerat quae itaque, aliquid labore voluptatibus! Atque, tenetur quod!`,
+  },
+  {
+    class: "job job-2",
+    company: "Blick Group",
+    job: "Graphic Designer",
+    summary: `Occaecat laboris occaecat eu ullamco officia consectetur aliqua
+      Lorem ipsum ut anim. Laborum qui eiusmod elit quis aute. Ex eu
+      adipisicing ipsum mollit voluptate minim ut officia voluptate aute
+      consectetur.`,
+  },
+  {
+    class: "job job-3",
+    company: "Zemlak - Stanton",
+    job: "Front-End Developer",
+    summary: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque
+    quisquam enim labore quam unde tenetur eum, ratione magni, nobis qui
+    iste?`,
+  },
+]
+const bioProps = {
+  class: "bio",
+  title: "BIO",
+  summary: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Perferendis, error! Atque fugit non beatae vero, repudiandae hic
+            sunt odit dolore sed eos similique nobis maxime tenetur! Esse itaque
+            unde animi.`,
+}
 
 const AboutPage = () => (
   <Layout>
@@ -15,44 +50,10 @@ const AboutPage = () => (
       <SubTitle subProps={subProps} />
       <div className="about-info">
         <div alt="personal photo" className="bio-image portriat" />
-        <div className="bio">
-          <h3 className="text-secondary">BIO</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Perferendis, error! Atque fugit non beatae vero, repudiandae hic
-            sunt odit dolore sed eos similique nobis maxime tenetur! Esse itaque
-            unde animi.
-          </p>
-        </div>
-        <div className="job job-1">
-          <h3>Jakubowski Inc.</h3>
-          <h6>Front-End Developer</h6>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi fugit
-            corporis, ipsam magni error laboriosam. Odit praesentium numquam
-            quas ipsam nostrum quaerat quae itaque, aliquid labore voluptatibus!
-            Atque, tenetur quod!
-          </p>
-        </div>
-        <div className="job job-2">
-          <h3>Blick Group</h3>
-          <h6>Graphic Designer</h6>
-          <p>
-            Occaecat laboris occaecat eu ullamco officia consectetur aliqua
-            Lorem ipsum ut anim. Laborum qui eiusmod elit quis aute. Ex eu
-            adipisicing ipsum mollit voluptate minim ut officia voluptate aute
-            consectetur.
-          </p>
-        </div>
-        <div className="job job-3">
-          <h3>Zemlak - Stanton</h3>
-          <h6>Front-End Developer</h6>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque
-            quisquam enim labore quam unde tenetur eum, ratione magni, nobis qui
-            iste?
-          </p>
-        </div>
+        <Bio bioProps={bioProps} />
+        <Jobs jobProps={jobProps[0]} />
+        <Jobs jobProps={jobProps[1]} />
+        <Jobs jobProps={jobProps[2]} />
       </div>
     </main>
   </Layout>
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,9 +3,24 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import PageTitle from "../components/PageTitle"
 import SubTitle from "../components/SubTitle"
+import ContactBox from "../components/ContactBox"
 
-const titleProps = ['Contact', 'Me'];
-const subProps = 'This is how you can reach me...';
+const titleProps = ["Contact", "Me"]
+const subProps = "This is how you can reach me..."
+const contactProps = [
+  {
+    type: "Email: ",
+    info: "johnny5@example.com",
+  },
+  {
+    type: "Phone: ",
+    info: "(555) 555-5555",
+  },
+  {
+    type: "Address: ",
+    info: "50 Center St, Boston NM 87101",
+  },
+]
 
 const ContactPage = () => (
   <Layout>
@@ -13,20 +28,7 @@ const ContactPage = () => (
     <main id="contact">
       <PageTitle titleProps={titleProps} />
       <SubTitle subProps={subProps} />
-      <div className="boxes">
-        <div>
-          <span className="text-secondary">Email: </span>
-          johnny5@example.com
-        </div>
-        <div>
-          <span className="text-secondary">Phone: </span>
-          (555) 555-5555
-        </div>
-        <div>
-          <span className="text-secondary">Address: </span>
-          50 Center St, Boston NM 87101
-        </div>
-      </div>
+      <ContactBox contactProps={contactProps} />
     </main>
   </Layout>
 )

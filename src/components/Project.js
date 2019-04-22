3ee2c1b01c61275464882dedd20aa51971576799
Dathan Spencer <dpspencer88@gmail.com>
2019-04-22 16:11:24 -0600

    Change 'a' to Link

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,25 +1,25 @@
 import React from "react"
+import { Link } from "gatsby"
 import { FaGithub, FaEye } from "react-icons/fa"
 
-const Project = (props) => (
-    <div className="projects">
-        {props.projectProps.map((project, i) => {
-            return(
-                <div className="item" key={i}>
-                    <a href={project.link}>
-                        <img id={project.imgId} src={project.src} alt={project.alt}/>
-                    </a>
-                    <a href={project.eyeLink} className={project.eyeClass}>
-                        <FaEye className={project.eyeIconClass} /> {project.eyeTitle}
-                    </a>
-                    <a href={project.hubLink} className={project.hubClass}>
-                        <FaGithub className={project.hubIconClass} /> {project.hubTitle}
-                    </a>
-                </div>
-            )
-        })}
-    </div>
+const Project = props => (
+  <div className="projects">
+    {props.projectProps.map((project, i) => {
+      return (
+        <div className="item" key={i}>
+          <Link to={project.link}>
+            <img id={project.imgId} src={project.src} alt={project.alt} />
+          </Link>
+          <Link to={project.eyeLink} className={project.eyeClass}>
+            <FaEye className={project.eyeIconClass} /> {project.eyeTitle}
+          </Link>
+          <Link to={project.hubLink} className={project.hubClass}>
+            <FaGithub className={project.hubIconClass} /> {project.hubTitle}
+          </Link>
+        </div>
+      )
+    })}
+  </div>
 )
 
-export default Project;
-
+export default Project

47a587c0d94a5ad08c73149a8d4ea4c3ce99db3b
Dathan Spencer <dpspencer88@gmail.com>
2019-04-23 14:45:22 -0600

    Change primary colors and favicon

--- src/components/seo.js
+++ src/components/seo.js
@@ -2,8 +2,9 @@ import React from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
-import favicon16 from '../images/favicon-16x16.png';
-import favicon32 from '../images/favicon-32x32.png';
+import favicon16 from '../images/blue-favicon.png';
+import favicon32 from '../images/blue-favicon.png';
+// import favicon32 from '../images/favicon-32x32.png';
 
 function SEO({ description, lang, meta, keywords, title }) {
   const { site } = useStaticQuery(
